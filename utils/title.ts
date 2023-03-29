d1fbc737b65b0736e12989cc9e59b06180b8af29
Daniel Yim <danielyim@gmail.com>
2023-03-28 21:14:23 -0700

    Comment

--- utils/title.ts
+++ utils/title.ts
@@ -1,2 +1,7 @@
+/**
+ * Fancy unicode titles.
+ *
+ * See https://en.wikipedia.org/wiki/List_of_Unicode_characters
+ */
 export const artgorithmsStyled = "◬ℝŧᡋ⚈яٱŧℏm₷";
 export const artgorithmsSimple = "◬ artgorithms ◎";
