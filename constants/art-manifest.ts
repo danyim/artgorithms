79703b835a8950656c4b6e578ffbc9a5bffc2e99
Daniel Yim <danielyim@gmail.com>
2023-04-04 11:19:01 -0700

    wip

--- constants/art-manifest.ts
+++ constants/art-manifest.ts
@@ -1,9 +1,20 @@
-interface ArtworkMetadata {
+export interface ArtworkMetadata {
   slug: string;
   path: string;
 }
 interface Manifest {
   artworks: ArtworkMetadata[];
+}
+export enum Artwork {
+  "WALL_1A",
+  "BROKEN_BANDS",
+  "COLOR_BANDS",
+  "CUBE_FORMS",
+  "FARBEN",
+  "WALL_370",
+  "WALL_610",
+  "WALL_1111",
+  "UNKNOWN",
 }
 
 /**
@@ -17,9 +28,35 @@ export const manifest: Manifest = {
     { slug: "cube-forms", path: "components/SolCubeForms" },
     { slug: "farben", path: "components/Farben" },
     { slug: "wall-370", path: "components/SolWall370" },
-    // { slug: "wall-565", path: "components/SolWall565" },
     { slug: "wall-610", path: "components/SolWall610" },
     { slug: "wall-1111", path: "components/SolWall1111" },
     { slug: "unknown", path: "components/Unknown" },
+    // { slug: "wall-565", path: "components/SolWall565" },
   ],
 };
+
+export const enumManifest: Record<Artwork, ArtworkMetadata> = {
+  [Artwork.WALL_1A]: { slug: "wall-1a", path: "components/SolWall1A" },
+  [Artwork.BROKEN_BANDS]: {
+    slug: "broken-bands",
+    path: "components/SolBrokenBands",
+  },
+  [Artwork.COLOR_BANDS]: {
+    slug: "color-bands",
+    path: "components/SolColorBands",
+  },
+  [Artwork.CUBE_FORMS]: { slug: "cube-forms", path: "components/SolCubeForms" },
+  [Artwork.FARBEN]: { slug: "farben", path: "components/Farben" },
+  [Artwork.WALL_370]: { slug: "wall-370", path: "components/SolWall370" },
+  [Artwork.WALL_610]: { slug: "wall-610", path: "components/SolWall610" },
+  [Artwork.WALL_1111]: { slug: "wall-1111", path: "components/SolWall1111" },
+  [Artwork.UNKNOWN]: { slug: "unknown", path: "components/Unknown" },
+  // { slug: "wall-565", path: "components/SolWall565" },
+};
+
+/**
+ * Converts a slug to an enum key
+ */
+export function slugToEnumKey(slug: string) {
+  return slug?.replaceAll("-", "_").toUpperCase();
+}
--- pages/art/[slug]/index.tsx
+++ pages/art/[slug]/index.tsx
@@ -2,23 +2,59 @@ import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import styles from "@/styles/Home.module.css";
 import React from "react";
-import { manifest } from "constants/art-manifest";
+import {
+  Artwork as ArtworkEnum,
+  manifest,
+  enumManifest,
+  ArtworkMetadata,
+  slugToEnumKey,
+} from "constants/art-manifest";
 import Head from "components/Head";
 import Loader from "components/Loader";
 import Header from "components/Header";
+import Footer from "components/Footer";
 
 export const Artwork = () => {
   const router = useRouter();
   const { slug } = router.query;
-  const artwork = manifest.artworks.find((value) => value.slug === slug);
-  // const artwork = manifest.artworks[0];
-  console.log("found artwork:", artwork);
+  if (Array.isArray(slug)) {
+    console.log("Slug is an array");
+    return;
+  }
+
+  const enumKey = ArtworkEnum[slugToEnumKey(slug)];
+  const artMetadata: ArtworkMetadata = enumManifest[enumKey];
+  if (!artMetadata) {
+    console.log(`Slug "${slug}" (${enumKey}) not defined in artwork manifest`);
+    return;
+  }
 
   const DynamicArtwork = dynamic(
     () => {
-      // console.log("inside", artwork.path);
-      // return import(`${artwork.path}`);
-      return import("components/SolColorBands");
+      // Since dynamic imports must be explicitly written, we'll define it here
+      switch (artMetadata.slug) {
+        case "wall-1a":
+          return import("components/SolWall1A");
+        case "broken-bands":
+          return import("components/SolBrokenBands");
+        case "color-bands":
+          return import("components/SolColorBands");
+        case "cube-forms":
+          return import("components/SolCubeForms");
+        case "farben":
+          return import("components/Farben");
+        case "wall-370":
+          return import("components/SolWall370");
+        case "wall-610":
+          return import("components/SolWall610");
+        case "wall-1111":
+          return import("components/SolWall1111");
+        case "unknown":
+          return import("components/Unknown");
+      }
+      console.log("inside", artMetadata?.path);
+      // return import(`${artMetadata?.path}`);
+      // return import("components/SolColorBands");
     },
     {
       loading: Loader,
@@ -32,6 +68,7 @@ export const Artwork = () => {
       <main className={styles.main}>
         <Header />
         <DynamicArtwork />
+        <Footer />
       </main>
     </div>
   );
